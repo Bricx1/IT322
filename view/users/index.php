08520f6bcf9ad47af1a4c1c1de0dea886390cd55
Bricx1 <dummy.dumm.acc001@gmail.com>
2025-03-28 17:24:16 +0800

    change it

--- view/users/index.php
+++ view/users/index.php
@@ -1,13 +1,11 @@
+
 <?php
+session_start();
 include("./includes/header.php");
 include("./includes/topbar.php");
 include("./includes/sidebar.php");
-?>
 
+include("./includes/dashboard.php");
 
-
-
-
-<?php
 include("./includes/footer.php");
 ?>
